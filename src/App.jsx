3feb0ea9968dd0a93373cf4e5c535dcd64f55ca4
EricChau18 <ericchaucs18@gmail.com>
2026-09-07 19:42:52 +0800

    Finish Cart and checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/Auth'
-import Checkout from './pages/Checkout'
-import Navbar from './components/Navbar'
-import './App.css'
-import AuthProvider from './context/AuthContext'
-import ProductDetails from './pages/ProductDetails'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import AuthProvider from "./context/AuthContext";
+import ProductDetails from "./pages/ProductDetails";
+import CartProvider from "./context/CartContext";
 
 function App() {
-
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/auth" element={<Auth></Auth>}></Route>
-          <Route path="/checkout" element={<Checkout></Checkout>}></Route>
-          <Route path='/products/:id' element={<ProductDetails></ProductDetails>}></Route>
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="app">
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/auth" element={<Auth></Auth>}></Route>
+            <Route path="/checkout" element={<Checkout></Checkout>}></Route>
+            <Route
+              path="/products/:id"
+              element={<ProductDetails></ProductDetails>}
+            ></Route>
+          </Routes>
+        </div>
+      </CartProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/data/product.js
+++ src/data/product.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: 1,
     name: "Wireless Headphones",
-    price: 99.99,
+    price: 799,
     image:
       "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&h=500&fit=crop",
     description:
@@ -11,7 +11,7 @@ const products = [
   {
     id: 2,
     name: "Smart Watch",
-    price: 249.99,
+    price: 1499,
     image:
       "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&h=500&fit=crop",
     description:
@@ -20,7 +20,7 @@ const products = [
   {
     id: 3,
     name: "Laptop Stand",
-    price: 49.99,
+    price: 349,
     image:
       "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?w=500&h=500&fit=crop",
     description:
@@ -29,7 +29,7 @@ const products = [
   {
     id: 4,
     name: "Mechanical Keyboard",
-    price: 129.99,
+    price: 899,
     image:
       "https://images.unsplash.com/photo-1541140532154-b024d705b90a?w=500&h=500&fit=crop",
     description:
@@ -38,7 +38,7 @@ const products = [
   {
     id: 5,
     name: "USB-C Hub",
-    price: 39.99,
+    price: 279,
     image:
       "https://images.unsplash.com/photo-1625842268584-8f3296236761?w=500&h=500&fit=crop",
     description:
@@ -47,7 +47,7 @@ const products = [
   {
     id: 6,
     name: "Wireless Mouse",
-    price: 29.99,
+    price: 209,
     image:
       "https://images.unsplash.com/photo-1527814050087-3793815479db?w=500&h=500&fit=crop",
     description:
@@ -56,7 +56,7 @@ const products = [
   {
     id: 7,
     name: "Monitor Stand",
-    price: 79.99,
+    price: 599,
     image:
       "https://images.unsplash.com/photo-1586953208448-b95a79798f07?w=500&h=500&fit=crop",
     description:
@@ -65,7 +65,7 @@ const products = [
   {
     id: 8,
     name: "Webcam HD",
-    price: 89.99,
+    price: 639,
     image:
       "https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?w=500&h=500&fit=crop",
     description:
